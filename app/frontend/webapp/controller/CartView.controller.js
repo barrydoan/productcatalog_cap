a916d0b71f608b85d5857b08b01bdc5b633da0bd
Nhat Doan <nhat.doan@sap.com>
2022-11-22 16:40:30 +0000

    add cart page

---
+++ app/frontend/webapp/controller/CartView.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "frontend/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    'sap/ui/core/Fragment',
+    'sap/ui/Device',
+    'sap/ui/model/Sorter',
+    'sap/ui/model/odata/v4/ODataModel'
+], function (BaseController, JSONModel, formatter, Filter, FilterOperator, Fragment, Device, Sorter, ODataModel) {
+    "use strict";
+
+    return BaseController.extend("frontend.controller.controller.CartView", {
+
+        formatter: formatter,
+
+        /* =========================================================== */
+        /* lifecycle methods                                           */
+        /* =========================================================== */
+
+        /**
+         * Called when the worklist controller is instantiated.
+         * @public
+         */
+        onInit: function () {
+            
+            
+            
+        },
+    });
+
+});
